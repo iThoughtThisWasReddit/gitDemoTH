4853c7f4782f5e8e87ac9cd56f6332f69ae3234c
iThoughtThisWasReddit <jakobhansen@jakobs-imac.lan>
2016-08-14 18:47:19 +0200

    Initial Commit

---
+++ gitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH
+//
+//  Created by Jakob Hansen on 14/08/16.
+//  Copyright © 2016 Jakob Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
